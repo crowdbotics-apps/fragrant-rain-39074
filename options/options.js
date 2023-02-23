c7703a0fc22a0021b8e7c757c880f8d41a9d6b09
Crowdbotics <team@crowdbotics.com>
2023-02-23 02:33:42 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "fragrant-rain-39074",
+  url: "https://fragrant-rain-39074.botics.co",
+  api: "https://fragrant-rain-39074.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
